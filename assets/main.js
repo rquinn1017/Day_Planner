6aed9a1457b4d2e25945dbfcc502a36d7947a7a9
Rob Quinn <Rob.M.Quinn@gmail.com>
2019-12-08 23:24:51 -0500

    testing isBefore isAfter and console logged current time vs all times on calendar

--- assets/main.js
+++ assets/main.js
@@ -1,51 +1,35 @@
+var currDate = moment().format('L');
+var currTime = moment().format('LTS');
+var currDateTime = currDate + ", " + currTime;
+console.log(currDateTime);
 
-console.log(moment().format('LTS'));
 
 $(document).ready(function() {
 
-    var row = $(".row-1").
-    text
-    ();
+    // var row = $(".row-1").
+    // text
+    // ();
 
-    var rowTime = moment(row, ["h"]).format("H:mm:ss A");
+    // var rowTime = moment(row, ["h"]).format("H:mm:ss A");
+    // console.log(rowTime);
 
-    console.log(rowTime);
-    // console.log(moment(row, 'LT').format());
 
-    // function getMomentFromTimeString(row) {
-    //     var t = moment(str, 'HH:mm A');
-    // }
-    // var time = moment(row);
-    // moment().toDate(row);
-// console.log(row);
     var currentDate = moment().format("dddd, MMM Do YYYY");
     $("#today" ).text
     ( currentDate );
 
-//    var time = moment().toDate(".row-1").value;
-//    console.log(time);
-
-//     var detailsClass = document.getElementsByClassName("details");
-
-//     $(detailsClass) .click(function(){
-//       var myFunction = function() {
-//         var time = this.toDate(".row").text;
-//         // console.log(detailsClass);
-//     };  
-//     Array.from(detailsClass).forEach(function(element) {
-//     console.log(detailsClass);
-//       });
-      
-//       });
-
-// var loop = $( ".row" ).each(function( index ) {
-//     console.log( index + ": " + $( this ).text() );
-//   });
-
 $( ".row" ).each(function( index ) {
   var time = ($( this ).text() );
-  console.log(moment(time,["h"]).format("H:mm:ss A"));
+  time= (moment(time,["h"]).format("H:mm:ss A"));
+  var dateTime = currDate + ", " + time;
+  
+  console.log(dateTime);
+
+  console.log(moment(dateTime).isBefore(currDateTime));
+
+  
   });
+
 
 })
 
