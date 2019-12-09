e51c22ce425aaf2dd06021813bc32ce849c84551
Rob Quinn <Rob.M.Quinn@gmail.com>
2019-12-08 21:34:12 -0500

    figured out how to convert my time on the left of my calendar to time format I'm using for current time to use to compare for past, present, future

--- assets/main.js
+++ assets/main.js
@@ -3,18 +3,41 @@ console.log(moment().format('LTS'));
 
 $(document).ready(function() {
 
-    var row = $(".row row-1").text;
-    // var time = moment(row);
-    console.log(row);
+    var row = $(".row-1").
+    text
+    ();
 
+    var dt = moment(row, ["h"]).format("H:mm:ss A");
+
+    console.log(dt);
+    // console.log(moment(row, 'LT').format());
+
+    // function getMomentFromTimeString(row) {
+    //     var t = moment(str, 'HH:mm A');
+    // }
+    // var time = moment(row);
+    // moment().toDate(row);
+console.log(row);
     var currentDate = moment().format("dddd, MMM Do YYYY");
     $("#today" ).text
     ( currentDate );
 
-    // $(".content").click(function () {
-    //     $(this).closest('div').find('.content').focus();
-    // });
+//    var time = moment().toDate(".row-1").value;
+//    console.log(time);
 
+//     var detailsClass = document.getElementsByClassName("details");
 
+//     $(detailsClass) .click(function(){
+//       var myFunction = function() {
+//         var time = this.toDate(".row").text;
+//         // console.log(detailsClass);
+//     };  
+//     Array.from(detailsClass).forEach(function(element) {
+//     console.log(detailsClass);
+//       });
+      
+//       });
 
 })
+
+
