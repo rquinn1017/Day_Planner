b9b3597a5e2ad35c25f1e323f68231bdd99a34cd
Rob Quinn <Rob.M.Quinn@gmail.com>
2019-12-08 20:22:17 -0500

    added save icon

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,19 @@
 
+console.log(moment().format('LTS'));
 
 $(document).ready(function() {
 
+    var row = $(".row row-1").text;
+    // var time = moment(row);
+    console.log(row);
 
     var currentDate = moment().format("dddd, MMM Do YYYY");
     $("#today" ).text
     ( currentDate );
+
+    // $(".content").click(function () {
+    //     $(this).closest('div').find('.content').focus();
+    // });
 
 
 
