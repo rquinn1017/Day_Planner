8d3dc1e8ce0007ef1c88c36fae9e951fba0f610c
Rob Quinn <Rob.M.Quinn@gmail.com>
2019-12-11 00:22:15 -0500

    updated CSS to add footer and updated color scheme, cleaned up formating and removed console.logs from js file and html, and updated ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Day_Planner
+
+This day planner is the ultimate way to plan your busy day. 
+
+The top of the page displays the current date.
+
+There is a grid for the whole work day (9AM-5PM).
+
+Based on the the current time the calender rows for each timeslot will become color coded. Red in the past, grey current hour, and green future hours.
+
+The user is able to click into any of the boxes in the middle column and enter notes. If the user clicks the save icon on that row, the data for that row will be sent to local storage.
+
+When the page is reloaded all of the notes for each row will be displayed on the screen from local storage.
--- assets/main.js
+++ assets/main.js
@@ -2,69 +2,67 @@
 var currHour = moment().format('HH');
 // var currHour = 15
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-   
-    var currentDate = moment().format("dddd, MMM Do YYYY");
-    $("#today" ).text
-    ( currentDate );
 
- var loop = 0
-$( ".row" ).each(function( index ) {
-  var time = ($( this ).text() );
-  time= (moment(time,["hA"]).format("HH"));
-  loop = loop + 1
+  var currentDate = moment().format("dddd, MMM Do YYYY");
+  $("#today").text
+    (currentDate);
 
-  if (time < currHour) 
-  $("#"+loop).css('background-color', '#ffb2b2');
+  var loop = 0
+  $(".row").each(function (index) {
+    var time = ($(this).text());
+    time = (moment(time, ["hA"]).format("HH"));
+    loop = loop + 1
 
-  else if (time > currHour) 
-  $("#"+loop).css('background-color', '#b2d8b2');
+    if (time < currHour)
+      $("#" + loop).css('background-color', '#ffb2b2');
 
-  else if (time == currHour) 
-  $("#"+loop).css('background-color', '#cccccc');
+    else if (time > currHour)
+      $("#" + loop).css('background-color', '#b2d8b2');
+
+    else if (time == currHour)
+      $("#" + loop).css('background-color', '#cccccc');
 
   });
-  
-  $('.save').hover(function() {
-    $(this).css('cursor','pointer');
+
+  $('.save').hover(function () {
+    $(this).css('cursor', 'pointer');
   })
 
-  $('.save').click(function(){
+  $('.save').click(function () {
     var row = $("#" + this.id);
     var rowNum = $(row).attr('id');
-    var noteText = $("#"+rowNum).html();
-    localStorage.setItem("details-"+rowNum, noteText);
+    var noteText = $("#" + rowNum).html();
+    localStorage.setItem("details-" + rowNum, noteText);
+    alert("Entry saved to local storage");
   })
 
 
-var storage = Object.entries(localStorage);
-console.log(storage);
+  var storage = Object.entries(localStorage);
 
-var details1 = (localStorage.getItem("details-1")) || [];
-var details2 = (localStorage.getItem("details-2")) || [];
-var details3 = (localStorage.getItem("details-3")) || [];
-var details4 = (localStorage.getItem("details-4")) || [];
-var details5 = (localStorage.getItem("details-5")) || [];
-var details6 = (localStorage.getItem("details-6")) || [];
-var details7 = (localStorage.getItem("details-7")) || [];
-var details8 = (localStorage.getItem("details-8")) || [];
-var details9 = (localStorage.getItem("details-9")) || [];
+  var details1 = (localStorage.getItem("details-1")) || [];
+  var details2 = (localStorage.getItem("details-2")) || [];
+  var details3 = (localStorage.getItem("details-3")) || [];
+  var details4 = (localStorage.getItem("details-4")) || [];
+  var details5 = (localStorage.getItem("details-5")) || [];
+  var details6 = (localStorage.getItem("details-6")) || [];
+  var details7 = (localStorage.getItem("details-7")) || [];
+  var details8 = (localStorage.getItem("details-8")) || [];
+  var details9 = (localStorage.getItem("details-9")) || [];
 
 
-$(".details-1").html(details1);
-$(".details-2").html(details2);
-$(".details-3").html(details3);
-$(".details-4").html(details4);
-$(".details-5").html(details5);
-$(".details-6").html(details6);
-$(".details-7").html(details7);
-$(".details-8").html(details8);
-$(".details-9").html(details9);
+  $(".details-1").html(details1);
+  $(".details-2").html(details2);
+  $(".details-3").html(details3);
+  $(".details-4").html(details4);
+  $(".details-5").html(details5);
+  $(".details-6").html(details6);
+  $(".details-7").html(details7);
+  $(".details-8").html(details8);
+  $(".details-9").html(details9);
 
-console.log(details9);
 
-  
 });
 
 
