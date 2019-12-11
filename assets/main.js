d0e9a0e6042e8623535cececd20a3761bdf78af4
Rob Quinn <Rob.M.Quinn@gmail.com>
2019-12-10 22:43:41 -0500

    got set to local storage working. Have it set to check which row the note is in and save it to local storage with an id of Note (rowNum)

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,6 @@
 
 var currHour = moment().format('HH');
-var currHour = 15
-
-// console.log(currHour);
+// var currHour = 15
 
 $(document).ready(function() {
 
@@ -16,7 +14,6 @@ $( ".row" ).each(function( index ) {
   var time = ($( this ).text() );
   time= (moment(time,["hA"]).format("HH"));
   loop = loop + 1
-//  console.log(loop);
 
   if (time < currHour) 
   $("#"+loop).css('background-color', '#ffb2b2');
@@ -27,28 +24,22 @@ $( ".row" ).each(function( index ) {
   else if (time == currHour) 
   $("#"+loop).css('background-color', '#cccccc');
 
-  // console.log(time);
-
   });
+  
   $('.save').hover(function() {
     $(this).css('cursor','pointer');
   })
 
   $('.save').click(function(){
-    var rowNum = $("#" + this.id);
-  console.log(rowNum);
-  //   var noteText = $(".details-"+rowNum).attr("text");
-  //   localStorage.setItem("Note"+rowNum, noteText);
-  //   console.log(noteText)
+    var row = $("#" + this.id);
+    var rowNum = $(row).attr('id');
+    var noteText = $("#"+rowNum).html();
+    localStorage.setItem("note "+rowNum, noteText);
+
   }
   
   )
-  //need to add event listener for click in the details box and set the number of the row 
   
-  
-  // $('.save').click(function() {
-  //   localStorage.setItem("mostRecentScore", score);
-
   
 });
 
